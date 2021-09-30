45e7f32b755d1eafc4375c60ce720e498e7417f1
Kunalw007 <kunalwadhwani7@gmail.com>
2021-09-30 17:19:45 +0530

    First commit

--- login.php
+++ login.php
@@ -10,25 +10,35 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
 <title>Welcome to Finance Portal</title>
 
-<link rel="stylesheet" href="assests/css/style.css">
+<link rel="stylesheet" href="loginStyle.css">
 
 </head>
 <body>
 <div class="signup-form">
     <form action="loginProcess.php" method="post" enctype="multipart/form-data">
-		<h2>Login</h2>
-		<p class="hint-text">Enter Login Details</p>
-        <div class="form-group">
-        	<input type="email" class="form-control" name="email" placeholder="Email" required="required">
+    <div class="head">
+		<h2>EduCARE <section> Helping Hands for learners </section> </h2>
+    </div>
+
+    <div class="row">
+            <p class="cls">Login To EduCARE</p>
+        <div class="row1" >
+            <div class="emailpass">Enter your email and password below</div>
+            <div class="txt">EMAIL</div>
+        	<input type="email" class="row1" name="email" placeholder="Email address" required="required">
         </div>
-		<div class="form-group">
-            <input type="password" class="form-control" name="pass" placeholder="Password" required="required">
+		<div class="row1">
+            <div class="txt">PASSWORD</div>
+            <input type="password" class="row1" name="pass" placeholder="Password" required="required">
         </div>
-		<div class="form-group">
-            <button type="submit" name="save" class="btn btn-success btn-lg btn-block">Login</button>
+		<div class="row1">
+            <button type="submit" name="save" class="btn">Login</button>
         </div>
-        <div class="text-center">Don't have an account? <a href="register.php">Register Here</a></div>
-    </form>
+        <div class="row1">Don't have an account? <a href="register.php" class="row2">Sign up</a></div>
 </div>
+    </div>
+  </form>
+</div>
+
 </body>
 </html>
